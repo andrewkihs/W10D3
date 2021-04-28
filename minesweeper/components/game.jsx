3bdd6c2a7f51a30213b58ce7e00f7a9215be28a6
jessicaUP <uphoff.jessica@gmail.com>
2021-04-28 11:42:10 -0400

    Set up Webpack & Game

---
+++ minesweeper/components/game.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as Minesweeper from 'minesweeper';
+
+class Game extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default Game;
---
+++ minesweeper/components/react_minesweeper.jsx
@@ -0,0 +1,10 @@
+//entry
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded",() => {
+  const root = document.getElementById("root");
+  console.log(root);
+  ReactDOM.render(<div></div>, root);
+})
